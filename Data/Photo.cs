efe874561095e0efe4553955f8867e99ff601f53
MyCatAteAReptile <114024484+MyCatAteAReptile@users.noreply.github.com>
2025-09-19 18:36:29 +0500

    initialize task structure

---
+++ Data/Photo.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace MyPhotoshop
+{
+	public class Photo
+	{
+		public int width;
+		public int height;
+		public Pixel[,] data;
+	}
+
+	public class Pixel
+	{
+		public double R { get; set; }
+		public double G { get; set; }
+		public double B { get; set; }
+
+		public Pixel(double R = 0, double G = 0, double B = 0) { 
+			this.R = R;
+			this.G = G;
+			this.B = B;
+		}
+
+		public static Pixel operator* (Pixel p, double d)
+		{
+			return new Pixel(p.R * d, p.G * d, p.B * d);
+		}
+
+	}
+}
+
