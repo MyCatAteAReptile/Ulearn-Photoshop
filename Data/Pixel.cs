754665422e9a150cfd1aaa9b07974da40fd62592
MyCatAteAReptile <114024484+MyCatAteAReptile@users.noreply.github.com>
2025-09-20 05:20:06 +0500

    task2: add Pixel properties check

--- Data/Pixel.cs
+++ Data/Pixel.cs
@@ -1,12 +1,36 @@
 using System;
+using System.Runtime.InteropServices;
 
 namespace MyPhotoshop
 {
     public class Pixel
     {
-        public double R { get; set; }
-        public double G { get; set; }
-        public double B { get; set; }
+        double _r;
+        public double R { 
+            get {  return _r; } 
+            set
+            {
+                _r = CheckChannelValue(value);
+            } 
+        }
+        double _g;
+        public double G
+        {
+            get { return _g; }
+            set
+            {
+                _g = CheckChannelValue(value);
+            }
+        }
+        double _b;
+        public double B
+        {
+            get { return _b; }
+            set
+            {
+                _b = CheckChannelValue(value);
+            }
+        }
 
         public Pixel(double R = 0, double G = 0, double B = 0)
         {
@@ -15,11 +39,16 @@ namespace MyPhotoshop
             this.B = B;
         }
 
-        public static Pixel operator *(Pixel p, double d)
+        public static Pixel operator* (Pixel p, double d)
         {
             return new Pixel(p.R * d, p.G * d, p.B * d);
         }
 
+        private double CheckChannelValue(double value) {
+            if (value < 0 || value > 1)
+                throw new ArgumentException("Channel value out of range", nameof(value));
+            return value;
+        }
     }
 }
 
